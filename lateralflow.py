68b9fd2b8b6f73bce6e764326aaf8b9477e4e898
Route49 <sgp3@rice.edu>
2015-11-05 16:16:31 -0600

    lateralflow.py updated

--- lateralflow.py
+++ lateralflow.py
@@ -10,6 +10,7 @@ import time
 import picamera
 import numpy as np
 import cv2
+import matplotlib.pyplot as plt
 
 #------------Take picture with pi camera--------------
 #camera = picamera.PiCamera()
@@ -31,50 +32,119 @@ cv2.imshow('image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-
 #-------------Extract ROI-----------------------------
-#template = img[50:200, 50:300]
-template = cv2.imread('WTControl.tif', 0)
-#template = cv2.imread('MntControl.tif', 0)
-#template = 'DoubleControl.tif'
-cv2.imshow('template', template)
+templateWT = cv2.imread('WTControl.tif', 0)	#wild type control image
+templateMnt = cv2.imread('MntControl.tif', 0)	#mutant control image
+
+cv2.imshow('templateWT', templateWT)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+cv2.imshow('templateMnt', templateMnt)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 #-------------Template Matching-----------------------
 #width and height of ROI
-w,h = template.shape[: : -1]
+wWT,hWT = templateWT.shape[: : -1]
+wMnt,hMnt = templateMnt.shape[: : -1]
 
 #resulting template matching image and the area in the original image that matches
 #res = cv2.matchTemplate(img, template, eval('cv2.TM_CCOEFF'))
-res = cv2.matchTemplate(img, template, eval('cv2.TM_CCOEFF_NORMED'))
+resWT = cv2.matchTemplate(img, templateWT, eval('cv2.TM_CCOEFF_NORMED'))
 #res = cv2.matchTemplate(img, template, eval('cv2.TM_CCORR'))
 #res = cv2.matchTemplate(img, template, eval('cv2.TM_CCORR_NORMED'))
 #res = cv2.matchTemplate(img, template, eval('cv2.TM_SQDIFF'))
 #res = cv2.matchTemplate(img, template, eval('cv2.TM_SQDIFF_NORMED'))
-min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-top_left = max_loc
-bottom_right = (top_left[0]+w, top_left[1]+h)
+resMnt = cv2.matchTemplate(img, templateMnt, eval('cv2.TM_CCOEFF_NORMED'))
+
+min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(resWT)
+top_leftWT = max_loc
+bottom_rightWT = (top_leftWT[0]+wWT, top_leftWT[1]+hWT)
+
+min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(resMnt)
+top_leftMnt = max_loc
+bottom_rightMnt = (top_leftWT[0]+wMnt, top_leftWT[1]+hMnt)
 
 #drawing a rectangle around the proper area in the original image and displaying it
 img2 = img.copy()
-cv2.rectangle(img2, top_left, bottom_right, 0, 2)
-cv2.imshow('image3', img2)
+cv2.rectangle(img2, top_leftWT, bottom_rightWT, 0, 2)
+cv2.imshow('WT Rect', img2)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
+#draw rect around area on bottom
+img2 = img.copy()
+cv2.rectangle(img2, top_leftMnt, bottom_rightMnt, 0, 2)
+cv2.imshow('Mnt Rect', img2)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 #------------Histogram Equalization-------------------
-ROIimg = img[top_left[1]:top_left[1]+h, top_left[0]:top_left[0]+w]
-cv2.imshow('ROI', ROIimg)
+ROIimgWT = img[top_leftWT[1]:top_leftWT[1]+hWT, top_leftWT[0]:top_leftWT[0]+wWT]
+cv2.imshow('ROI WT', ROIimgWT)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-equ = cv2.equalizeHist(ROIimg)
-cv2.imshow('Equalized Hist', ROIimg)
+equWT = cv2.equalizeHist(ROIimgWT)
+cv2.imshow('Equal Hist WT', equWT)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
+equTempWT = cv2.equalizeHist(templateWT)
+cv2.imshow('Equ Hist WT Temp', equTempWT)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+ROIimgMnt = img[top_leftMnt[1]:top_leftMnt[1]+hMnt, top_leftMnt[0]:top_leftMnt[0]+wMnt]
+cv2.imshow('ROI Mnt', ROIimgMnt)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+equMnt = cv2.equalizeHist(ROIimgMnt)
+cv2.imshow('Equal Hist Mnt', equMnt)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+equTempMnt = cv2.equalizeHist(templateMnt)
+cv2.imshow('Equ Hist Mnt Temp', equTempMnt)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 #------------Extract Intensities along line----------
+lineWT = equWT[hWT/2, 0:wWT]
+templatelineWT = equTempWT[hWT/2, 0:wWT]
 
+lineMnt = equMnt[hMnt/2, 0:wMnt]
+templatelineMnt = equTempMnt[hMnt/2, 0:wMnt]
+
+plt.plot(lineWT)
+plt.show()
+
+plt.plot(lineMnt)
+plt.show()
+
+count = 0
+linesumWT = 0
+templatesumWT = 0
+linesumMnt = 0
+templatesumMnt = 0
+
+for i in range(200):
+     linesumWT = linesumWT + lineWT[i]
+     templatesumWT = templatesumWT + templatelineWT[i]
+     linesumMnt = linesumMnt + lineMnt[i]
+     templatesumMnt = templatesumMnt + templatelineMnt[i]
+print linesumWT
+print templatesumWT
+print linesumMnt
+print templatesumMnt
+
+changeWT = linesumWT/templatesumWT
+if changeWT < 0.75:
+     print 'positive WT sample'
+
+changeMnt = linesumMnt/templatesumMnt
+if changeMnt < 0.75:
+     print 'positive Mnt sample'
